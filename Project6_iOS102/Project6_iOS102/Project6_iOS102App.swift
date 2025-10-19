79f34367adfd896b29778f73169451c4f8e537bf
Mario Casas <MCasas548@gmail.com>
2025-10-18 21:44:10 -0400

    Initial commit

---
+++ Project6_iOS102/Project6_iOS102/Project6_iOS102App.swift
@@ -0,0 +1,17 @@
+//
+//  Project6_iOS102App.swift
+//  Project6_iOS102
+//
+//  Created by Mario Casas on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Project6_iOS102App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
